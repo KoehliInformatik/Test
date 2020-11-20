ec1edacf68acd2b2a9a5e57d25ab9082a76de6b4
DaniyalDehghany <60738393+DaniyalDehghany@users.noreply.github.com>
2020-11-20 16:48:34 +0100

    Second just update but using input only !!!

--- Test Project/Creatures.vb
+++ Test Project/Creatures.vb
@@ -2,7 +2,7 @@
 
 Public MustInherit Class Creatures
 
-    Protected health As Single = 20
+    Protected health As Single = 200
     Protected attackDamage As Single = 3.7F
     Protected armor As Single = 5
     Protected name As String = "Gobi"
